13e2acddf627ffdb906a72d64a02bd541fcc0637
zcy831 <zcy831@hotmail.com>
2022-08-21 16:22:31 +0800

    app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/info')
+def hello_world():
+    # return 'Hello World'
+    return 'new content'
+
+
+if __name__ == '__main__':
+    print('starting server..')
+    app.run()
